41619d5cdfa983fdc31339fa6774ecfa5a5bc5b5
Jovins <jovinscoder@gmail.com>
2021-09-18 19:15:56 +0800

    add ShopUI

---
+++ ShopUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ShopUI
+//
+//  Created by Jovins on 2021/9/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        BaseView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShopUI/ShopUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopUIApp.swift
+//  ShopUI
+//
+//  Created by Jovins on 2021/9/18.
+//
+
+import SwiftUI
+
+@main
+struct ShopUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
